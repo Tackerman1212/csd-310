99d2798e8a5b856587564be73afd8506700a691c
Tackerman1212 <tackerman1212@gmail.com>
2022-11-15 14:14:31 -0600

    Added print statement for formatting puposes

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -2,4 +2,5 @@ from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.dvcnh3g.mongodb.net/?retryWrites=true&w=majority";
 client = MongoClient(url)
 db = client.pytech
+print("-- Pytech Collection List --")
 print(db.list_collection_names())
