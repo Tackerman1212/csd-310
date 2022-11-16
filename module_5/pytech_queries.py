b72ebe7c676d5e8b7dcf18c7ae45aa39dbf954c3
Tackerman1212 <tackerman1212@gmail.com>
2022-11-15 22:34:10 -0600

    assignment 5.3

---
+++ module_5/pytech_queries.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.dvcnh3g.mongodb.net/?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.pytech
+students = db.students
+
+find_docs = db.students.find({})
+
+print("--  DISPLAYING STUDENT DOCUMENTS FROM find() QUERY --")
+
+print(find_docs)
+
+find_one_docs = db.students.find_one({"student_id" : "1007"})
+
+
+print("--  DISPLAYING STUDENT DOCUMENTS FROM find_one() QUERY --")
+
+print(find_one_docs)
