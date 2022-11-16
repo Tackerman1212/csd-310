66e223926307ee1ce7408b950eda7ab643f2df98
Tackerman1212 <tackerman1212@gmail.com>
2022-11-15 22:00:48 -0600

    new file - assignment 5.3

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.dvcnh3g.mongodb.net/?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.pytech
+students = db.students
+
+
+thorin = {
+    "student_id" : "1007",
+    "first_name" : "Thorin",
+    "last_name" : "Oakenshield"
+}
+
+thorin_student_id = students.insert_one(thorin).inserted_id
+
+
+bilbo = {
+    "student_id" : "1008",
+    "first_name" : "Bilbo",
+    "last_name" : "Baggins"
+}
+
+bilbo_student_id = students.insert_one(bilbo).inserted_id
+
+
+frodo = {
+        "student_id" : "1009",
+    "first_name" : "Frodo",
+    "last_name" : "Baggins"
+}
+
+frodo_student_id = students.insert_one(frodo).inserted_id
+
+
+print("--  INSERT STATEMENTS --")
+print("Inserted student record Thorin Okenshield into the students collection with document_id", thorin_student_id) 
+print("Inserted student record Bilbo Baggins into the students collection with document_id", bilbo_student_id)
+print("Inserted student record Frodo Baggins into the students collection with document_id", frodo_student_id)
